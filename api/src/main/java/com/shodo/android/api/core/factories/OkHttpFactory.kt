fd04f1013a8b4e378fab31f75da5c6f51b4360a0
Fabrice <f.rakotonarivo@ext.go-electra.com>
2026-04-10 20:25:10 +0200

    [Fix] fix crash on Koin on Builder HTTP

--- api/src/main/java/com/shodo/android/api/core/factories/OkHttpFactory.kt
+++ api/src/main/java/com/shodo/android/api/core/factories/OkHttpFactory.kt
@@ -2,11 +2,9 @@ package com.shodo.android.api.core.factories
 
 import okhttp3.OkHttpClient
 import okhttp3.OkHttpClient.Builder
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
 import java.util.concurrent.TimeUnit.MINUTES
 
-object OkHttpFactory : KoinComponent {
+object OkHttpFactory {
 
     fun okHttpClient(): OkHttpClient {
         return clientBuilder()
@@ -14,7 +12,7 @@ object OkHttpFactory : KoinComponent {
     }
 
     private fun clientBuilder(): Builder {
-        return get<Builder>()
+        return Builder()
             .connectTimeout(2, MINUTES)
             .readTimeout(2, MINUTES)
             .writeTimeout(2, MINUTES)
